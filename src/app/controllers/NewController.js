dc82989773959491f572b66c5afde7c78a354b15
mapuuu <phutoannguyen2271@gmail.com>
2022-09-19 16:42:59 +0700

    routers & controllers

---
+++ src/app/controllers/NewController.js
@@ -0,0 +1,13 @@
+class NewsController {
+
+    // [Get /news]
+    index(req, res) {        
+        res.render('news');
+    }
+
+    // [Get /news/:slug]
+    show(req, res) {
+        res.send('NEW DETAIL!!!');
+    }
+}
+module.exports = new NewsController;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(morgan('combined'));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+
+// app.use(morgan('combined'));
 
 //Template engine
 app.engine('hbs', handlebars.engine({
@@ -18,8 +26,7 @@ app.set('view engine', 'hbs');
 
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+//Routes init
+route(app);
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
