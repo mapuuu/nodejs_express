bfedfe69483d64186bdec439f63f8a80374bc070
mapuuu <phutoannguyen2271@gmail.com>
2022-09-21 14:44:58 +0700

    CRUD_Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,27 @@
+const Course = require('../models/Course');
+const {mutipleMongooseToObject} = require('../../util/mongoose');
+
 class SiteController {
     // [Get /]
-    index(req, res) {
-        res.render('home');
+    index(req, res, next) {
+
+        // Ex1: Callback
+        // Course.find({}, function(err, courses) {
+        //     if(!err) {
+        //         res.json(courses);
+        //     } else{
+        //         next(err);
+        //     }
+        // });
+
+        // Ex2:  Promiss
+        Course.find({}) 
+            .then(courses => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses)
+                });
+            })
+            .catch(next);
     }
 
     // [Get /search]
