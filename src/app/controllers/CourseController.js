cfe650e8e82287ed1e3985d9cf7b66d039bb4692
mapuuu <phutoannguyen2271@gmail.com>
2022-09-23 22:59:23 +0700

    edit hbs

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -26,7 +26,7 @@ class CourseController {
         course
             .save()
             .then(() => res.redirect('/me/stored/courses'))
-            .catch((error) => {});
+            .catch(next);
 
         // res.send('COURSE SAVED!!!');
         res.render('courses/create');
