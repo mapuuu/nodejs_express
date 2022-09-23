4242e7f98fe7b366905008c09c1120f52c16cfed
mapuuu <phutoannguyen2271@gmail.com>
2022-09-23 22:11:47 +0700

    Sort middleware

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const handlebars = require('express-handlebars');
+const SortMiddleware = require('./app/middlewares/SortMiddleware');
+
 const app = express();
 const port = 3000;
 
 app.use(methodOverride('_method'));
+
+//Custom middleware
+app.use(SortMiddleware);
 
 const route = require('./routes');
 
@@ -33,6 +38,28 @@ app.engine(
         extname: '.hbs',
         helpers: {
             sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
+
+                const icons = {
+                    default: 'fa-solid fa-sort',
+                    asc: 'fa-solid fa-arrow-down-short-wide"',
+                    desc: 'fa-solid fa-arrow-down-wide-short',
+                };
+
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                };
+
+                const icon = icons[sortType];
+                const type = types[sortType];
+
+                return `<a href="?_sort&column=${field}&type=${type}" style="color: #000">
+                        <i class="${icon}"></i>
+                    </a>`;
+            },
         },
     }),
 );
